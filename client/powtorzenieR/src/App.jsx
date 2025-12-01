3a506a9a05ecba95e296c31a2ad2ccc458d8995d
Nicola Kaleta <99048749+nic00la1@users.noreply.github.com>
2025-12-01 09:36:17 +0100

    Create App.jsx

---
+++ client/powtorzenieR/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { NicolaKaleta } from './components/NicolaKaleta'
+import { DisplayNameAndSurnameByProps } from './components/DisplayNameAndSurnameByProps'
+import { DisplayDataFromTableByProps } from './components/DisplayDataFromTableByProps'
+
+function App() {
+
+  return (
+    <>
+    <div className='container'>
+      <NicolaKaleta/>
+      <DisplayNameAndSurnameByProps person={{
+        name: 'Nicola',
+        surname: 'Kaleta'
+      }}/>
+      <DisplayDataFromTableByProps/>
+      <div>4</div>
+      <div>5</div>
+      <div>6</div>
+      <div>7</div>
+      <div>8</div>
+      <div>9</div>
+    </div>
+    </>
+  )
+}
+
+export default App
