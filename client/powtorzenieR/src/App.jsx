bfd70e0660efe1edda27ae463413e3bc57b2347d
Nicola Kaleta <99048749+nic00la1@users.noreply.github.com>
2025-12-05 10:36:16 +0100

    komponent 4 + info

--- client/powtorzenieR/src/App.jsx
+++ client/powtorzenieR/src/App.jsx
@@ -2,22 +2,38 @@ import './App.css'
 import { NicolaKaleta } from './components/NicolaKaleta'
 import { DisplayNameAndSurnameByProps } from './components/DisplayNameAndSurnameByProps'
 import { DisplayDataFromTableByProps } from './components/DisplayDataFromTableByProps'
+import { DisplayObjectsFromTableByProps } from './components/DisplayObjectsFromTableByProps';
+import React, { useState } from 'react'; 
 
 function App() {
 
+// Do komponentu 4   
+const [info, setInfo] = useState("");
+
 const zwierzeta = ["lew", "kot", "tygrys", "pantera", "pies"];
-  
+
+const zwierzetaObiekty = [
+  {Zwierzeta: "lew", ilosc: 2},
+  {Zwierzeta: "kot", ilosc: 5},
+  {Zwierzeta: "tygrys", ilosc: 1},
+  {Zwierzeta: "pantera", ilosc: 3},
+  {Zwierzeta: "pies", ilosc: 4},
+];
+
   return (
     <>
+     {/* Info na samej górze */}
+    <h1>{info}</h1>
+
     <div className='container'>
       <NicolaKaleta/>
       <DisplayNameAndSurnameByProps person={{
         name: 'Nicola',
         surname: 'Kaleta'
       }}/>
-      <DisplayDataFromTableByProps/>
-      <div>4</div>
-      <div>5</div>
+      <DisplayDataFromTableByProps items={zwierzeta} />
+      <DisplayObjectsFromTableByProps animals={zwierzetaObiekty} />
+      
       <div>6</div>
       <div>7</div>
       <div>8</div>
